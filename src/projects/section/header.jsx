357334e3d1884a49a9d83a703642d32e23941f2e
py <pyhuang97@gmail.com>
2025-05-22 15:13:59 +0800

    [update] remove legacy components and code.

--- src/projects/section/header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-    return (
-        <div className="header">
-            <div className="button">Projects</div>
-        </div>
-    );
-}
